59bc31b3cec6b673ed06205f8d4098e34e8bf951
Emir Kovacevic <emirkovacevic92@gmail.com>
2019-03-09 00:05:20 +1000

    added: Entity Models

--- LibraryData/Models/Patron.cs
+++ LibraryData/Models/Patron.cs
@@ -9,9 +9,12 @@ namespace LibraryData.Models
         public int Id { get; set; }
         public string FirstName { get; set; }
         public string LastName { get; set; }
-        public string  Address { get; set; }
+        public string Address { get; set; }
         public DateTime DateOfBirth { get; set; }
         public string TelephoneNumber { get; set; }
+
+        public virtual LibraryCard LibraryCard { get; set; }
+        public virtual LibraryBranch HomeLibraryBranch { get; set; }
 
     }
 }
