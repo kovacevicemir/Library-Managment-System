4017b49601ff5a3763ad35737d8a0a9116da0ec5
Emir Kovacevic <emirkovacevic92@gmail.com>
2019-03-08 20:17:21 +1000

    Add project files.

---
+++ Library Managment System/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Library_Managment_System.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
